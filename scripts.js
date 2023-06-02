c7cf6bf93bda315ebdab2292a5f7cb86a96cb4fc
yanrobert10 <yan39robert@gmail.com>
2023-06-02 14:17:10 -0300

    add header

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// Header Page
+
+const hambuger = document.querySelector('.hambuger');
+const navMenu = document.querySelector('.nav-menu');
+
+hambuger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hambuger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll('.nav-link');
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hambuger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+// Home Page
