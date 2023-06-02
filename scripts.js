a532382b5904b2337a87c9cbe2637273ac9a5c24
yanrobert10 <yan39robert@gmail.com>
2023-06-02 14:32:18 -0300

    add script home page

--- scripts.js
+++ scripts.js
@@ -19,3 +19,11 @@ function closeMenu() {
 }
 
 // Home Page
+function img(anything) {
+    document.querySelector('.slide').src = anything;
+}
+
+function change(change) {
+    const line = document.querySelector('.image');
+    line.style.background = change;
+}
